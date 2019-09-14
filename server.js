6d708e54f6657010130bf5a2c2c05249e471e935
Paul CARTAU <pcartau@student.42.fr>
2019-09-14 16:15:54 +0200

    add mongo database connection

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+/* eslint-disable import/no-dynamic-require */
 /* eslint-disable no-console */
+const mongoPass = require(`${process.env.PWD}/src/mongoPass.js`);
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+
+/* ---------------------------DATABASE------------------------------*/
+const password = process.env.MONGO_PASS || mongoPass;
+const uri = `mongodb+srv://admin:${password}@cluster0-s0dow.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 
 /* ---------------------------MIDDLEWARES---------------------------*/
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,8 +24,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 
-/* ---------------------------ROUTES---------------------------*/
-// eslint-disable-next-line import/no-dynamic-require
+/* ---------------------------ROUTES--------------------------------*/
 require(`${process.env.PWD}/routes/routes.js`)(app);
 
 
