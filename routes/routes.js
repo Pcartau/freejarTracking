299e89294b4ff1bb9ae2e720b90143e29ab75613
Paul CARTAU <pcartau@student.42.fr>
2019-09-15 11:26:37 +0200

    add routes :
    -/login
    -/dashboard

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,12 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.sendFile(`${process.env.PWD}/views/index.html`);
   });
+
+  app.get('/login', (req, res) => {
+    res.sendFile(`${process.env.PWD}/views/login.html`);
+  });
+
+  app.get('/dashboard', (req, res) => {
+    res.sendFile(`${process.env.PWD}/views/dashboard.html`);
+  });
 };
