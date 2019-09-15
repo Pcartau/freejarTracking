5f5e80c2fa4d49f41a8343967aba633d07067b7b
Paul CARTAU <pcartau@student.42.fr>
2019-09-15 11:09:28 +0200

    change const name value to dynamic name value UserSchema

--- src/dbQueries/userQueries.js
+++ src/dbQueries/userQueries.js
@@ -1,11 +1,11 @@
 const User = require('../dbSchema/User.js');
 
-async function findOrCreate(userId) {
+async function findOrCreate(userId, userName) {
   const user = await User.findOne({ userId });
   if (user) return user;
   return new User({
     userId,
-    userName: 'Meh',
+    userName,
   }).save()
     .then((newUser) => newUser)
     .catch((e) => { throw new Error(e); });
